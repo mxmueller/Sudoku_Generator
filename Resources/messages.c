874b1e7b2887fbfbef981def73b6cd208c72d906
Maximilian Müller <maximilian.mueller@mail.ch>
2023-02-09 13:48:36 +0100

    first stable version, undo is already implemented

---
+++ Resources/messages.c
@@ -0,0 +1,9 @@
+/*
+    Author: 1081078
+    Baden-Wuerttemberg Cooperative State University
+    Projekt: messages.c
+    Datum: 01.02.2023
+*/
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
